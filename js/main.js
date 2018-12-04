5b09008faeae040ad43b8ab7d17b81a3f74d45a6
damiadek <damiadek2009@gmail.com>
2018-12-04 19:43:59 +0100

    fixed some mobile responsive issues

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-  $(window).scroll(function() {
-    $scroll = $(window).scrollTop();
-    // console.log($scroll);
+  // $(".navbar-sticky").slideUp();
+
+  $(".header-logo .dropdown, .nav-links > a").click(function() {
+    $(".navbar-sticky").slideToggle();
+  });
+
+  $(".nav-links > a").click(function(e) {
+    // e.preventDefault();
+    var href = $(this).attr("href");
   });
 });
